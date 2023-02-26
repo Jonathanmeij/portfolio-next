9782a8108e116f146250583c43cea8296abcdb44
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-02-26 21:16:15 +0100

    card hover effect

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -3,9 +3,11 @@ import { useInView } from "react-intersection-observer";
 export default function Card({
     children,
     hasAnimation,
+    className,
 }: {
     children: React.ReactNode;
     hasAnimation?: boolean;
+    className?: string;
 }) {
     const { ref, inView } = useInView({
         threshold: 0.3,
@@ -13,7 +15,9 @@ export default function Card({
     });
 
     if (!hasAnimation) {
-        <div className="h-full overflow-hidden bg-gray-900 border shadow-lg border-gray-850 rounded-xl">
+        <div
+            className={`h-full overflow-hidden bg-gray-900 border shadow-lg border-gray-850 rounded-xl ${className}`}
+        >
             {children}
         </div>;
     }
@@ -23,9 +27,11 @@ export default function Card({
             ref={ref}
             className={`transition duration-500 ${
                 inView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"
-            }`}
+            } }`}
         >
-            <div className="h-full overflow-hidden bg-gray-900 border shadow-lg border-gray-850 rounded-xl">
+            <div
+                className={`h-full overflow-hidden bg-gray-900 border shadow-lg border-gray-850 rounded-xl ${className}`}
+            >
                 {children}
             </div>
         </div>
