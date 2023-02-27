60802154d0a4cb5c13efffafcc25ed96193417a3
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-02-27 22:35:54 +0100

    divider

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -60,6 +60,7 @@ export default function Navbar() {
                     </Container>
                     <MobileMenuButton />
                 </div>
+                <Divider />
             </nav>
         </>
     );
