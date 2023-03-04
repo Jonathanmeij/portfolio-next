a542df88760ac55143ce80d1c875240ac5251761
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-03-04 12:02:40 +0100

    added api request and forms

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,7 +1,45 @@
 import Button from "@/components/ui/Button";
 import Container from "@/components/ui/Container";
+import { Input, TextArea } from "@/components/ui/Input";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Contact() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+
+    const onSubmit = async (data) => {
+        setLoading(true);
+
+        const res = await fetch("/api/sendgrid", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: data.Name,
+                email: data.Email,
+                message: data.Message,
+            }),
+        });
+
+        if (res.status === 200) {
+            setSuccess(true);
+        }
+
+        if (res.status === 500 || res.status === 400) {
+            setError(true);
+        }
+
+        setLoading(false);
+    };
+
     return (
         <section id="contact" className="py-28">
             <Container maxWidth="xl" className="flex flex-col gap-8 m-auto">
@@ -11,67 +49,59 @@ export default function Contact() {
                         Get in touch with me via email or social media.
                     </p>
                 </div>
-                <form className="flex flex-col gap-4">
-                    <div>
-                        <label
-                            htmlFor="name"
-                            className="block mb-2 text-sm font-medium text-white"
-                        >
-                            Name
-                        </label>
-                        <input
-                            type="text"
-                            id="name"
-                            className=" text-sm rounded-lg border border-gray-850 block w-full p-2.5 bg-gray-800  placeholder-gray-400 text-white focus:border-blue-500"
-                            placeholder="name"
-                        />
+                {success && (
+                    <div className="p-4 bg-green-500 rounded-lg">
+                        <p className="font-semibold text-white">
+                            Message sent successfully!
+                        </p>
                     </div>
+                )}
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+                    <Input
+                        name="Name"
+                        register={register}
+                        error={errors.Name?.message?.toString()}
+                        options={{
+                            required: {
+                                value: true,
+                                message: "Name is required",
+                            },
+                        }}
+                        type="text"
+                        placeholder="Name"
+                    />
 
-                    <div>
-                        <label
-                            htmlFor="email-address-icon"
-                            className="block mb-2 text-sm font-medium text-white"
-                        >
-                            Email
-                        </label>
+                    <Input
+                        type="text"
+                        name="Email"
+                        register={register}
+                        error={errors.Email?.message?.toString()}
+                        options={{
+                            required: {
+                                value: true,
+                                message: "Email is required",
+                            },
+                        }}
+                        placeholder="name@mail.com"
+                    />
 
-                        <div className="relative">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg
-                                    aria-hidden="true"
-                                    className="w-5 h-5 text-gray-400"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                                </svg>
-                            </div>
-                            <input
-                                type="text"
-                                id="email-address-icon"
-                                className=" text-sm rounded-lg border border-gray-850 block w-full pl-10 p-2.5 bg-gray-800  placeholder-gray-400 text-white focus:border-blue-500"
-                                placeholder="name@mail.com"
-                            />
-                        </div>
-                    </div>
+                    <TextArea
+                        type="textarea"
+                        name="Message"
+                        register={register}
+                        error={errors.Message?.message?.toString()}
+                        options={{
+                            required: {
+                                value: true,
+                                message: "Message is required",
+                            },
+                        }}
+                        placeholder="Your message"
+                    />
 
-                    <div>
-                        <label
-                            htmlFor="message"
-                            className="block mb-2 text-sm font-medium text-white"
-                        >
-                            Message
-                        </label>
-                        <textarea
-                            id="message"
-                            rows={4}
-                            className="text-sm rounded-lg border border-gray-850 block w-full p-2.5 bg-gray-800  placeholder-gray-400 text-white focus:border-blue-500"
-                            placeholder="Your message"
-                        ></textarea>
-                    </div>
-                    <Button color="primary">Send</Button>
+                    <Button type="submit" color="primary">
+                        Send
+                    </Button>
                 </form>
             </Container>
         </section>
