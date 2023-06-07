18e77d490e2116f7fc918be457867f31949cb1d2
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-06-07 17:21:45 +0200

    fixed text and added scroll arrow

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -31,15 +31,15 @@ export default function Header() {
         >
             <Container className="flex flex-col items-center justify-center w-full h-full gap-8 m-auto">
                 <h1
-                    className={`flex flex-col items-center gap-3 transition ease-in-out delay-100 duration-1000 ${
+                    className={`flex flex-col items-center md:gap-3 transition ease-in-out delay-100 duration-1000 ${
                         inView ? "opacity-100" : "opacity-0"
                     }`}
                 >
                     <span className="text-2xl tracking-wide text-blue-400">
                         Hey, I am Jonathan
                     </span>
-                    <span className="text-5xl font-bold tracking-wide md:text-6xl">
-                        Web Developer
+                    <span className="text-[2.6rem] text-center font-bold tracking-wide md:text-6xl">
+                        Web-developer
                     </span>
                     <span className="text-2xl font-medium tracking-wide md:text-3xl">
                     </span>
@@ -61,6 +61,10 @@ export default function Header() {
                     {/*>*/}
                     {/*    Contact*/}
                     {/*</Button>*/}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 animate-pulse duration-400">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+                    </svg>
+
                 </div>
             </Container>
         </div>
